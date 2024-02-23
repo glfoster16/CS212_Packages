1e6fbe50aa7528605dd03659de9b919b544ec352
glfos <glfoster@loyola.edu>
2024-02-22 21:56:10 -0500

    Added Javadocs to checkSign.java

--- src/userInput/checkSign.java
+++ src/userInput/checkSign.java
@@ -1,7 +1,20 @@
 package userInput;
 
+/**
+ * This class is designed to house methods that will check the sign of numbers
+ * For these purposes, sign is positive/negative
+ * Each method also includes a boolean to decide whether to include zero or exclude zero in its calculations
+ * @author Gabriel Foster
+ * @version 1.0
+ */
 public class checkSign {
 
+    /**
+     * Determines if a double is positive
+     * @param number The double being checked for its sign
+     * @param includeZero A boolean which decides whether to include or exclude zero in the method's calculations
+     * @return True if number is positive, false if negative
+     */
     public static boolean isPositive(double number, boolean includeZero){
 
         if (includeZero){
@@ -13,6 +26,12 @@ public class checkSign {
 
     }
 
+    /**
+     * Determines if a float is positive
+     * @param number The float being checked for its sign
+     * @param includeZero A boolean which decides whether to include or exclude zero in the method's calculations
+     * @return True if number is positive, false if negative
+     */
     public static boolean isPositive(float number, boolean includeZero){
 
         if (includeZero){
@@ -24,6 +43,12 @@ public class checkSign {
 
     }
 
+    /**
+     * Determines if an integer is positive
+     * @param number The integer being checked for its sign
+     * @param includeZero A boolean which decides whether to include or exclude zero in the method's calculations
+     * @return True if number is positive, false if negative
+     */
     public static boolean isPositive(int number, boolean includeZero){
 
         if (includeZero){
@@ -35,6 +60,12 @@ public class checkSign {
 
     }
 
+    /**
+     * Determines if a long is positive
+     * @param number The long being checked for its sign
+     * @param includeZero A boolean which decides whether to include or exclude zero in the method's calculations
+     * @return True if number is positive, false if negative
+     */
     public static boolean isPositive(long number, boolean includeZero){
 
         if (includeZero){
@@ -46,6 +77,12 @@ public class checkSign {
 
     }
 
+    /**
+     * Determines if a double is negative
+     * @param number The double being checked for its sign
+     * @param includeZero A boolean which decides whether to include or exclude zero in the method's calculations
+     * @return True if number is negative, false if positive
+     */
     public static boolean isNegative(double number, boolean includeZero){
 
         if (includeZero){
@@ -57,6 +94,12 @@ public class checkSign {
 
     }
 
+    /**
+     * Determines if a float is negative
+     * @param number The float being checked for its sign
+     * @param includeZero A boolean which decides whether to include or exclude zero in the method's calculations
+     * @return True if number is negative, false if positive
+     */
     public static boolean isNegative(float number, boolean includeZero){
 
         if (includeZero){
@@ -68,6 +111,12 @@ public class checkSign {
 
     }
 
+    /**
+     * Determines if an integer is negative
+     * @param number The integer being checked for its sign
+     * @param includeZero A boolean which decides whether to include or exclude zero in the method's calculations
+     * @return True if number is negative, false if positive
+     */
     public static boolean isNegative(int number, boolean includeZero){
 
         if (includeZero){
@@ -79,6 +128,12 @@ public class checkSign {
 
     }
 
+    /**
+     * Determines if a long is negative
+     * @param number The long being checked for its sign
+     * @param includeZero A boolean which decides whether to include or exclude zero in the method's calculations
+     * @return True if number is negative, false if positive
+     */
     public static boolean isNegative(long number, boolean includeZero){
 
         if (includeZero){
